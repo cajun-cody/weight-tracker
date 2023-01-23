2b8f41261698c534dbb4afdcf3ed59184f9c33bc
Cody <cemt5462@gmail.com>
2023-01-23 16:25:37 -0600

    Created form for adding new submissions.

---
+++ src/Components/AddEntry/AddEntryForm.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+
+const AddEntryForm = (props) => {
+
+    const [weight, setWeight] = useState(0);     //Const opens a new array to hold all of the entries. Bind them 
+    const [date, setDate] = useState('');        //to thier inputs below by adding value. 
+
+    function handleSubmit(event) {              //This function will keep the form from refresshing each time it is submitted. 
+        event.preventDefault();
+        let newEntry = {
+            weight: weight,
+            date: date
+        };
+        console.log(newEntry)
+    }
+
+    return ( 
+        <form onSubmit={handleSubmit}>          
+            <label>Weight</label>
+            <input type='number' value={weight} onChange={(event) => setWeight(parseFloat(event.target.value))}/>  
+            <label>Date</label>
+            <input type='date' value={date} onChange={(event) => setDate(event.target.value)}/>
+            <button type='submit'>Add</button>
+        </form>
+     );
+}
+ 
+export default AddEntryForm;
