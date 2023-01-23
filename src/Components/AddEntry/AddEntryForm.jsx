f218f72ed1c0fe0e231ad7e8680b1c0151dcaae5
Cody <cemt5462@gmail.com>
2023-01-23 17:05:16 -0600

    Completed functionality on addEntryForm.

--- src/Components/AddEntry/AddEntryForm.jsx
+++ src/Components/AddEntry/AddEntryForm.jsx
@@ -6,13 +6,14 @@ const AddEntryForm = (props) => {
     const [weight, setWeight] = useState(0);     //Const opens a new array to hold all of the entries. Bind them 
     const [date, setDate] = useState('');        //to thier inputs below by adding value. 
 
-    function handleSubmit(event) {              //This function will keep the form from refresshing each time it is submitted. 
+    function handleSubmit(event) {              //This function will keep the form from refreshing each time it is submitted. 
         event.preventDefault();
         let newEntry = {
             weight: weight,
             date: date
         };
         console.log(newEntry)
+        props.addNewEntryProperty(newEntry)
     }
 
     return ( 
